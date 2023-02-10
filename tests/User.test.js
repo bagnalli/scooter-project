59856e48edcc5cf42b4e7327517a5a24d5cff993
bagnalli <stevenbagnall@outlook.com>
2023-02-10 11:53:27 +0000

    first draft diagrams, user property tests completed

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,25 @@
-const User = require('../src/User')
+const User = require("../src/User");
 
 // User tests here
+describe("User class tests", () => {
+  const test1 = new User("testName", "password123", 20, true);
 
-// test username
+  // test username
+  test("test username", () => {
+    expect(test1.username).toBe("testName");
+  });
 
-// test password
-
-// test age
-
-// test login
-
-// test logout
+  // test password
+  test("test password", () => {
+    expect(test1.password).toBe("password123");
+  });
+  // test age
+  test("test age", () => {
+    expect(test1.age).toBe(20);
+  });
+  // test login
+  test("test loggedIn", () => {
+    expect(test1.loggedIn).toBe(true);
+  });
+  // test logout
+});
