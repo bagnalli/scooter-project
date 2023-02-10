c8ba0cdfec20df4ea52ec61ecea840d7ebe093e3
bagnalli <stevenbagnall@outlook.com>
2023-02-10 14:29:21 +0000

    scooter property tests

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,12 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  constructor(station, user, serial, nextSerial, charge, isBroken) {
+    this.station = station;
+    this.user = user;
+    this.serial = serial;
+    this.nextSerial = nextSerial;
+    this.charge = charge;
+    isBroken = isBroken;
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,43 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe("Scooter Properties", () => {
+  const testScooter = new Scooter("Salford", "bagnalli1", 1, 1, 100, false);
 
-//Method tests
-describe('scooter methods', () => {
-  // tests here!
+  // OBJECT TEST
+  test("is scooter object", () => {
+    expect(typeof testScooter).toEqual("object");
+  });
+  // STATION TEST
+  test("is there a station?", () => {
+    expect(testScooter.station).toBe("Salford");
+  });
+  // USER TEST
+  test("is there a user?", () => {
+    expect(testScooter.user).toBe("bagnalli1");
+  });
+  // SERIAL TEST
+  test("Serial", () => {
+    expect(testScooter.serial).toBe(1);
+  });
+  // NEXT SERIAL TEST
+  test("nextSerial", () => {
+    expect(testScooter.nextSerial).toBe(1);
+  });
+  // CHARGE TEST
+  test("charge", () => {
+    expect(testScooter.charge).toBe(100);
+  });
+});
 
-  //rent method
+// IS BROKEN TEST
 
-  //dock method
-
-  //requestRepair method
-
-  //charge method
-
-})
+// //Method tests
+// describe("scooter methods", () => {
+//   // tests here!
+//   //rent method
+//   //dock method
+//   //requestRepair method
+//   //charge method
+// });
